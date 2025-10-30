b9cec9517167eed1decc0683695e695542658a1c
Rishabh Maru <148260712+Rishabhmaru01@users.noreply.github.com>
2025-10-30 23:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.getElementById('buttons');
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+
+  buttons.appendChild(btn);
+
+  const audio = document.createElement('audio');
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+});
+
+// Stop Button
+const stopBtn = document.createElement('button');
+stopBtn.classList.add('btn', 'stop');
+stopBtn.innerText = 'stop';
+stopBtn.addEventListener('click', stopSounds);
+buttons.appendChild(stopBtn);
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
